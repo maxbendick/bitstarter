03251af1db47b1791c8e388bdc035ae09fbf1be8
Max Bendick <maxbendick@gmail.com>
2013-08-18 22:02:40 +0000

    Fourth attempt at reading from file

--- web.js
+++ web.js
@@ -1,21 +1,31 @@
 var express = require('express');
-
-
-
-
-//attempts to read file
 var fs = require('fs');
-fs.readFileSync('inxdex.html', "utf-8");
-//^^attempts to read file
+
+var data = fs.readFileSync("index.html", "utf-8");
+
+var app = express.createServer(express.logger());
+app.get('/', function(request, response) {
+  response.send(data);
+});
+var port = process.env.PORT || 5000;
+app.listen(port, function() {
+  console.log("Listening on " + port);
+});
 
 
+
+
+/*
+var express = require('express');
 var app = express();
 app.use(express.logger());
-app.get('/', function(request, response) { 
-  response.send(data);
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+*/
