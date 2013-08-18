363b5ee8fd757e3349dc2dc6ada1529ffaca7e67
Max Bendick <maxbendick@gmail.com>
2013-08-18 22:10:05 +0000

    Cleaned up web.js

--- web.js
+++ web.js
@@ -11,21 +11,3 @@ var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
-
-
-
-
-/*
-var express = require('express');
-var app = express();
-app.use(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
-*/
